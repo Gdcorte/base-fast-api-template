e7c74efeeba8f9e6d99fa9bd86d714aff894067a
Gustavo Diniz da Corte <gustavodinizdacorte@Gustavos-MacBook-Pro.local>
2022-12-04 13:03:36 +0900

    update docs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from healthcheck import healthcheck_routes
+from healthcheck.routes import healthcheck_openapi_tags
 
 
 # Preconfigure app
@@ -15,7 +16,25 @@ def prepare_environment():
 def prepare_app():
     prepare_environment()
 
-    app = FastAPI()
+    tags = [healthcheck_openapi_tags()]
+
+    app = FastAPI(
+        title="ERP System - Backend",
+        description="ERP System",
+        version="0.0.1",
+        terms_of_service="http://coming.soon.com",
+        contact={
+            "name": "Gustavo & Julio",
+            "url": "http://gdcorte.com",
+            "email": "quiezidiniz@gmail.com",
+        },
+        license_info={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+        openapi_tags=tags,
+        # openapi_url=None,
+    )
     healthcheck_routes(app)
 
     return app
--- healthcheck/routes.py
+++ healthcheck/routes.py
@@ -1,7 +1,16 @@
+from typing import Any, Dict
+
 from fastapi import FastAPI
 
 from healthcheck.api import healthcheck_router
 
 
+def healthcheck_openapi_tags() -> Dict[str, Any]:
+    return {
+        "name": "Healthcheck",
+        "description": "Check if the server is running.",
+    }
+
+
 def healthcheck_routes(app: FastAPI) -> None:
-    app.include_router(healthcheck_router)
+    app.include_router(healthcheck_router, tags=["Healthcheck"])
